5d35d1f2e62dc466251c87d2208c4207ca913e12
aryavhir <aryavhirkoul1@gmail.com>
2026-03-25 12:23:06 +0530

    done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -49,7 +49,7 @@ function Home() {
 
           <div className="hero-actions-container animate-on-scroll delay-2">
             <div className="decentralization-link-wrapper">
-              <Link to="/decentralization" className="explore-button"  style={{ backgroundColor: '#ffffff', color: '#0f4a4a' }}>
+              <Link to="/decentralization" className="explore-button" style={{ backgroundColor: '#ffffff', color: '#0f4a4a' }}>
                 <span className="explore-button__icon-wrapper" aria-hidden="true">
                   <svg viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="explore-button__icon-svg" width={12}>
                     <path d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z" fill="currentColor" />
